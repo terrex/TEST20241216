f405f3407d8404eb4f67c7bcbdb8bb5abf24d36a
Guillermo Gutiérrez Herrera <guillermo.gutierrez@senado.es>
2024-12-18 11:12:03 +0100

    prueba de casos raros

--- leap-year/src/test/java/demo/leapyear/LeapServiceTest.java
+++ leap-year/src/test/java/demo/leapyear/LeapServiceTest.java
@@ -1,6 +1,7 @@
 package demo.leapyear;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -35,5 +36,49 @@ class LeapServiceTest {
     @DisplayName("Comprueba la regla de ser divisible por 4 y 100 y no por 400")
     void reglaDiv4Y100no400() {
         assertFalse(leapService.isLeapYear(1800));
+    }
+
+    @Nested
+    @DisplayName("Pruebas de casos raros")
+    class CasosRaros {
+
+        @Test
+        @DisplayName("Pasar null lanza excepción")
+        void nullLanzaExcepcion() {
+            assertThrows(NullPointerException.class, () -> leapService.isLeapYear(null));
+        }
+
+        @Test
+        @DisplayName("Prueba el año 0")
+        void anio0() {
+            assertTrue(leapService.isLeapYear(0));
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "-1,false",
+                "-100,false",
+                "-399,false",
+                "-400,true",
+                "-401,false"
+        })
+        @DisplayName("Prueba años negativos")
+        void aniosNegativos(Integer anio, Boolean resultadoEsperado) {
+            assertEquals(resultadoEsperado, leapService.isLeapYear(anio));
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "1,false",
+                "100,false",
+                "399,false",
+                "400,true",
+                "401,false"
+        })
+        @DisplayName("Prueba años menores que 400")
+        void aniosMenores400(Integer anio, Boolean resultadoEsperado) {
+            assertEquals(resultadoEsperado, leapService.isLeapYear(anio));
+        }
+
     }
 }
