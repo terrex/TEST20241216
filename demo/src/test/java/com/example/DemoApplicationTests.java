aca00b18994f2e34cf9c60783b1b6643d8fd3b9f
Javier <jmartin@grupoloyal.com>
2024-12-18 09:31:04 +0100

    GildedRose

--- demo/src/test/java/com/example/DemoApplicationTests.java
+++ demo/src/test/java/com/example/DemoApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+//@Import(TestcontainersConfiguration.class)
+//@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
