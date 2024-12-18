15fd16d687bd36aa2dc9887e8b98155bbba9295b
Guillermo Gutiérrez Herrera <guillermo.gutierrez@senado.es>
2024-12-18 14:08:24 +0100

    Con approvals

--- demo/src/test/java/com/gildedrose/GildedRoseTest.java
+++ demo/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,22 +1,15 @@
 package com.gildedrose;
 
 import com.example.DemoApplication;
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = DemoApplication.class)
 class GildedRoseTest {
-
-    @Value("classpath:fixtures/before_my_changes.csv")
-    Resource beforeMyChanges;
 
     @Test
     void foo() {
@@ -28,15 +21,10 @@ class GildedRoseTest {
 
     @Test
     @DisplayName("Verifica que el funcionamiento no cambia antes de las modificaciones pedidas el 18/12/2024")
-    void verificaReglasExistentesAntesDeYoTocarNada() throws IOException {
+    void verificaReglasExistentesAntesDeYoTocarNada() {
         GildedRose app = new GildedRose(ItemsToCSV.generateSamples());
         app.updateQuality();
-        String actual = ItemsToCSV.toCSV(app.items);
-        String expected;
-        try (InputStream expectedStream = beforeMyChanges.getInputStream()) {
-            expected = new String(expectedStream.readAllBytes());
-        }
-        assertEquals(expected, actual);
+        Approvals.verify(ItemsToCSV.toCSV(app.items));
     }
 
 }
---
+++ demo/src/test/java/com/gildedrose/PackageSettings.java
@@ -0,0 +1,6 @@
+package com.gildedrose;
+
+public class PackageSettings
+{
+  public static String ApprovalBaseDirectory = "../resources";
+}
